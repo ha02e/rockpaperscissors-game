1d06a9aff9f52b3948fafea8c49bc077c3ddf518
hayoung <ha02e@naver.com>
2024-03-19 13:05:16 +0900

    Box 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import Box from "./component/Box";
+
+//1. 박스 3개 나/결과/pc (타이틀, 사진, 결과)
+//2. 가위 바위 보 버튼
+//3. 버튼을 클릭하면 클릭한 값이 박스에 보임
+//4. pc는 랜덤하게 값을 선택한다
+//5. 3번 4번 결과를 가지고 승패를 따진다
+//6. 승패 결과에 따라 테두리 색이 바뀐다.
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="main">
+      <Box title="ME" />
+      <Box title="RESULT" />
+      <Box title="COMPUTER" />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/component/Box.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Box = (props) => {
+  return (
+    <div className="box">
+      <h1>{props.title}</h1>
+      <img
+        className="item-img"
+        src={process.env.PUBLIC_URL + "images/paper.png"}
+      />
+      <h2>WIN!</h2>
+    </div>
+  );
+};
+
+export default Box;
