f4ddacbd7a2e106ec16686caf7bdaa43cbfbbcf5
hayoung <ha02e@naver.com>
2024-03-19 15:31:31 +0900

    user 버튼 선택 시 이미지 보여주기 완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import "./App.css";
 import Box from "./component/Box";
 
 //1. 박스 3개 나/결과/pc (타이틀, 사진, 결과)
 //2. 가위 바위 보 버튼
-//3. 버튼을 클릭하면 클릭한 값이 박스에 보임
+//3. 버튼을 클릭하면 클릭한 값이 박스에 보임 - state
 //4. pc는 랜덤하게 값을 선택한다
 //5. 3번 4번 결과를 가지고 승패를 따진다
 //6. 승패 결과에 따라 테두리 색이 바뀐다.
@@ -24,17 +25,24 @@ const choice = {
 };
 
 function App() {
+  const [userSelect, setUserSelect] = useState(null);
+
+  const play = (userChoice) => {
+    // console.log(userChoice);
+    setUserSelect(choice[userChoice]);
+  };
+
   return (
     <div>
       <div className="main">
-        <Box title="ME" />
-        <Box title="RESULT" />
-        <Box title="COMPUTER" />
+        <Box title="ME" item={userSelect} />
+        {/* <Box title="RESULT" />
+        <Box title="COMPUTER" item={userSelect} /> */}
       </div>
       <div className="main">
-        <button>가위</button>
-        <button>바위</button>
-        <button>보</button>
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
       </div>
     </div>
   );
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,13 +1,11 @@
 import React from "react";
 
 const Box = (props) => {
+  console.log("props:", props);
   return (
     <div className="box">
       <h1>{props.title}</h1>
-      <img
-        className="item-img"
-        src={process.env.PUBLIC_URL + "images/paper.png"}
-      />
+      <img className="item-img" src={props.item && props.item.image} />
       <h2>WIN!</h2>
     </div>
   );
